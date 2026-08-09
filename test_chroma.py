bf2cc7a7618760f356fd9ed727a788d1c2500b6d
htmlcss <rabiahaq020@gmail.com>
2026-08-10 01:59:22 +0500

    Added Enterprise Knowledge Assistant

--- test_chroma.py
+++
@@ -1,7 +0,0 @@
-import chromadb
-
-client = chromadb.PersistentClient(path="./chroma_db")
-
-collection = client.get_collection("company_docs")
-
-print("Collection Count:", collection.count())
--- test_mistral.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-loaded = load_dotenv()
-
-print("Loaded:", loaded)
-print("Current Directory:", os.getcwd())
-print("API Key:", os.getenv("MISTRAL_API_KEY"))
